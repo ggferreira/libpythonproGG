d414247994c9a02d0341bbee0a36092444678f5b
ggferreira <Gg545800>
2021-03-09 16:04:14 -0300

    Melhorados os teste incluidos nesse modulo

--- libpythonproGG/tests/teste_spam/test_usuarios.py
+++ libpythonproGG/tests/teste_spam/test_usuarios.py
@@ -2,14 +2,17 @@ from libpythonproGG.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Gabriel')
+    usuario = Usuario(nome='Gabriel', email='gabriel@ferreira.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 
 def test_listar_usuario(sessao):
-    usuarios = [Usuario(nome='Gabriel'), Usuario(nome='Luciano')]
+    usuarios = [
+        Usuario(nome='Gabriel', email='gabriel@ferreira.com'),
+        Usuario(nome='Luciano', email='gabriel@ferreira.com'),
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
